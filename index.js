21f0dff161f141621d730ad41f8661f54d535a06
DongHun, Kim <donghun.kdh@gmail.com>
2021-12-31 06:24:58 +0900

    'studying'

--- index.js
+++ index.js
@@ -11,15 +11,65 @@ const client = new DiscordJS.Client({
   ]
 });
 
-client.on('ready', ()=> {
+client.on('ready', ()=>{
   console.log('복실이, 준비 완료!');
+  
+  const guildId = '';
+  const guild = client.guilds.cache.get(guildId);
+  let commands;
+
+  if(guild){
+    commands = guild.commands;
+  } else{
+    commands = client.application?.commands;
+    // optional chaining
+  }
+
+  commands?.create({
+    name: 'ame',
+    description: 'Ame Love'
+  });
+
+  commands?.create({
+    name: 'add',
+    description: 'Adds two numbers.',
+    options: [
+      {
+        name: 'num1',
+        description: 'The first number.',
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+      },
+      {
+        name: 'num2',
+        description: 'The second number.',
+        required: true,
+        type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+      }
+    ]
+  })
 });
 
-client.on('messageCreate', msg => {
-  if(msg.content === '공부하자'){
-    msg.reply({
-      content: 'https://www.youtube.com/watch?v=c00uuV1zPCs'
-    })
+client.on('interactionCreate', async (interaction)=>{
+  if(!interaction.isCommand()) {
+    return
+  }
+
+  const { commandName, options } = interaction;
+  // destructuring
+
+  if (commandName === 'ame'){
+    interaction.reply({
+      content: 'https://www.youtube.com/watch?v=c00uuV1zPCs\n흔들 아메~'
+    });
+  } else if (commandName  === 'add') {
+    const num1 = options.getNumber('num1');
+    const num2 = options.getNumber('num2');
+
+    interaction.reply({
+      content: `계산 결과는 ${num1 + num2}!`,
+      ephemeral: true
+    });
   }
 });
 
